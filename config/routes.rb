2063ec42bc83faacb492abd505ca94154fc31f34
Jenny <yjhong1004@gmail.com>
2019-03-06 22:49:07 -0800

    api implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
     resources :events, only: [:create]
   end
   resources :registered_applications
