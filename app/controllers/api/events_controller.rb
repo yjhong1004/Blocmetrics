3c719b0aa32a208d189f14a981a43f932ceb6576
Jenny <yjhong1004@gmail.com>
2019-03-08 14:57:11 -0800

    fixing errors in events controller

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,7 @@
 class API::EventsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
-  before_filter :set_access_control_headers
+  before_action :set_access_control_headers
 
   def set_access_control_headers
      headers['Access-Control-Allow-Origin'] = '*'
@@ -10,15 +10,22 @@ class API::EventsController < ApplicationController
   end
 
   def create
+    puts "creating event"
     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
     if registered_application
+      puts "found registered application"
       @event = registered_application.events.new(event_params)
+      puts "created event"
       if @event.save
+        puts "saved event"
         render json: @event, status: :created
       else
+        puts "there was an error"
+        puts @event.errors
         render json: {errors: @event.errors}, status: :unprocessable_entity
       end
     else
+      puts "could not find application"
       render json: "Unregistered application", status: :unprocessable_entity
     end
   end
