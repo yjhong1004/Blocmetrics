e7455560abc4c60d22b65f96e249c9dfd490964f
Jenny <yjhong1004@gmail.com>
2019-03-12 22:50:33 -0700

    changes to postgres

--- config.ru
+++ config.ru
@@ -4,4 +4,4 @@ require_relative 'config/environment'
 
 run Rails.application
 
-set :database, "postgres://localhost/postgres"
+#set :database, "postgres://localhost/postgres"
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_29_191200) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
