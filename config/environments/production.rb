5720c094ef107621aba8ccf787aadeb3edfd7936
Jenny <yjhong1004@gmail.com>
2019-05-28 13:40:16 -0700

    added heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.assets.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
